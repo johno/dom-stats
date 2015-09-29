3224d7765e2e7a853e72cbb72de2e64f829c7691
John Otander <johnotander@gmail.com>
2015-09-29 10:43:01 -0600

    Begin new implementation using got and cheerio

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-'use strict';
+'use strict'
 
-var jsdom = require('jsdom');
-var isUrl = require('is-url');
-var htmlTags = require('html-tags');
-var normalizeUrl = require('normalize-url');
-var isPresent = require('is-present');
+var got = require('got')
+var isUrl = require('is-url')
+var cheerio = require('cheerio')
+var htmlTags = require('html-tags')
+var isPresent = require('is-present')
+var normalizeUrl = require('normalize-url')
 
-module.exports = function domStats(urlOrHtml, options, callback) {
-  if (typeof urlOrHtml != 'string') {
-    throw new TypeError('dom-stats expected a string');
+module.exports = function domStats (urlOrHtml, options, callback) {
+  if (typeof urlOrHtml !== 'string') {
+    throw new TypeError('dom-stats expected a string')
   }
 
-  options = options || {};
-  callback = callback || function() {};
-
-  var jsdomOptions = {
-    done: function(error, window) {
-      var stats = analyzeDom(error, window, options);
-      callback(error, stats);
-    }
-  }
+  options = options || {}
+  callback = callback || function () {}
 
   if (isUrl(urlOrHtml)) {
-    jsdomOptions.url = urlOrHtml;
+    got(urlOrHtml, function (error, body, response) {
+      if (isPresent(body)) {
+        var stats = analyzeDom(cheerio.load(body), options)
+        callback(null, stats)
+      }
+    })
   } else {
-    jsdomOptions.html = urlOrHtml;
+    var stats = analyzeDom(cheerio.load(urlOrHtml), options)
+    callback(null, stats)
   }
-
-  jsdom.env(jsdomOptions);
 }
 
-function analyzeDom(error, window, options) {
-  options = options || {};
+function analyzeDom(dom, options) {
+  options = options || {}
 
   var stats = {
     totalTags: 0,
@@ -41,51 +39,51 @@ function analyzeDom(error, window, options) {
     duplicateIds: [],
     duplicateIdsCount: 0,
     tagCounts: {}
-  };
+  }
 
-  var ids = {};
-  var classes = {};
+  var ids = {}
+  var classes = {}
 
   htmlTags.forEach(function(tag) {
-    var tags = window.document.getElementsByTagName(tag);
-    var tagCount = tags.length;
+    var tags = window.document.getElementsByTagName(tag)
+    var tagCount = tags.length
 
     if (options.ignoreZeroCounts && tagCount == 0) {
-      return;
+      return
     }
 
-    stats.totalTags += tagCount;
-    stats.tagCounts[tag] = tagCount;
+    stats.totalTags += tagCount
+    stats.tagCounts[tag] = tagCount
 
     [].slice.call(tags).forEach(function(tag) {
       // Get class statistics
-      var classList = (tag.getAttribute('class') || '').split(/\s+/);
+      var classList = (tag.getAttribute('class') || '').split(/\s+/)
       if (isPresent(classList)) {
-        stats.totalClasses += classList.length;
+        stats.totalClasses += classList.length
       }
 
       if (isPresent(tag.id)) {
         if (ids[tag.id]) {
-          ids[tag.id]++;
+          ids[tag.id]++
         } else {
-          ids[tag.id] = 1;
+          ids[tag.id] = 1
         }
       }
-    });
-  });
+    })
+  })
 
   // Create additional id statistics
   Object.keys(ids).forEach(function(id) {
-    stats.totalIds += ids[id];
+    stats.totalIds += ids[id]
     if (ids[id] > 1) {
-      stats.duplicateIds.push(id);
-      stats.duplicateIdsCount++;
+      stats.duplicateIds.push(id)
+      stats.duplicateIdsCount++
     }
-  });
+  })
 
   if (stats.totalTags) {
-    stats.averageClassCount = stats.totalClasses/stats.totalTags;
+    stats.averageClassCount = stats.totalClasses/stats.totalTags
   }
 
-  return stats;
+  return stats
 }
